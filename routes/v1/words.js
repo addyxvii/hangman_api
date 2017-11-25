5007527c9d6dcf7df2d70268b9d9b23775fd6432
Addison <addisonbennett@gmail.com>
2017-11-25 12:08:00 -0700

    stubbed out routes and added words controller file

--- routes/v1/words.js
+++ routes/v1/words.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('hello');
+});
+
+router.get('/:word_id', function(req, res, next){
+
+});
+
+router.post('/', function(req, res, next){
+  res.send(200);
+});
+
+router.put('/:word_id', function(req, res, next){
+  res.send(200)
+});
+
+router.delete('/:word_id', function(req, res, next){
+  res.send(200);
 });
 
 // router.get('/', (req, res) => {
